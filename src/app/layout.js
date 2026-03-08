4cf1feaeff4442693359beae75389ee36de4f637
James-T-S <James.Shipway@student.shu.ac.uk>
2026-03-08 12:43:42 +0000

    Lots of changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,20 +4,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="body">
-        <header>
-          <nav>
-            <a href="/">Home</a>
-            <a href="/projects">Projects</a>
-          </nav>
-        </header>
+        <nav>
+          <div className='navLinks'>
+          <a href="/">Home</a>
+          <a href="/projects">Projects</a>
+          </div>
+          <div className='navResume'>
+            <a href="/resume">Resume</a>
+          </div>
+        </nav>
         <main>
           {children}
         </main>
         <footer>
-          <p>© 2025 My Portfolio</p>
-          <a href="mailto:17jtshipway@gmail.com">Email</a>
-          <a href="https://github.com/JamesShipway">GitHub Account</a>
-          <a href="https://www.linkedin.com/in/james-shipway-61879a1a2/">LinkedIn Account</a>
+          <a href="mailto:17jtshipway@gmail.com"><img src="/emailLogo.svg" alt="Email Logo" /></a>
+          <a href="https://github.com/JamesShipway"><img src="/GithubLogo.svg" alt="GitHub Logo" /></a>
+          <a href="https://www.linkedin.com/in/james-shipway-61879a1a2/"><img src="/linkedinLogo.svg" alt="LinkedIn Logo" /></a>
         </footer>
       </body>
     </html>
---
+++ src/app/resume/page.js
@@ -0,0 +1,89 @@
+export default function index() {
+    return (
+        <div>
+            <h1>James Shipway</h1>
+            <p>
+                22 Chesterfield Road, Sheffield S26 4TL •
+                07429862409 •17jtshipway@gmail.com
+            </p>
+
+            <p>
+                My name is James Shipway. I am a second-year
+                Software Engineering student at Sheffield Hallam
+                University with a strong base of C# and the .Net
+                framework  looking for a placement in the tech field.
+                While on my course I have gained experience working with
+                agile  as a team and communicating effectively with clients.
+                I have strong interpersonal, problem-solving and teamwork
+                skills which I have  developed through academic and
+                practical work. I am eager to apply my skills in a
+                real-world setting and gain hands-on work experience.
+            </p>
+
+            <h2>Technical Skills</h2>
+            <p>
+                Languages:  C#,  HTML, Javascript, CSS, SQL, Python
+                Frameworks and Libraries:  .Net 8.0, ASP.Net Core, React, Razor Pages, WPF, Next.js
+                Tools and Methodologies:  Agile, Kanban, Scrum, Figma, Draw.io
+            </p>
+
+            <h2>Education</h2>
+            <p>
+                BEng Software Engineering
+                <br />
+                Sep 2024 – Present Sheffield Hallam University, Sheffield
+                <br />
+                <br />
+                Level 3 Extended Diploma in Games, Animation and VFX
+                <br />
+                Sep 2023 – May 2024 The Sheffield College, Sheffield
+                <br />
+                <br />
+                5 x GCSEs grade 4 or above, including Maths, English and Science
+                <br />
+                Sep 2015 – Jun 2021 Aston Academy, Sheffield
+            </p>
+
+            <h2>Projects</h2>
+            <h3>NHS Pathfinder</h3>
+            <p>
+                NHS PATHFINER is a web application designed to assist healthcare
+                professionals in diagnosing and treating patients. The app provides
+                a user-friendly interface for inputting patient symptoms and medical
+                history, and then uses an algorithm to suggest potential diagnoses
+                and treatment options. The goal of NHS PATHFINDER is to improve the
+                accuracy and efficiency of medical decision-making, ultimately leading
+                to better patient outcomes.
+            </p>
+            <h3>Ilet4You CRM</h3>
+            <p>
+                In my second year of University I created a CRM (client
+                relations manager) for a letting company based in Sheffield
+                called ‘iLet4You’. I worked within a team of six and used
+                WinForms to create the app. As a team we had weekly
+                production where progress was monitored and tasks were
+                distributed throughout the team. We also had weekly
+                meetings with the client to show the progress of the app
+                and to take any feedback.
+            </p>
+            <h3>Personal Dashboard</h3>
+            <p>
+                During the summer between my first and second year I worked
+                on a personal dashboard. I used WPF to create this because
+                it was something slightly new but still familiar to me because
+                I have used WinForms before. The dashboard has helped me to
+                quickly organise and access my university work, access the games
+                on my computer and access any other apps or websites.
+            </p>
+
+            <h2>Activities</h2>
+            <p>
+                Badminton
+                I play badminton at league level and I am coached to play in a team. I enjoy playing against different teams from around Sheffield. I have found playing badminton to be a great way of meeting new people and expanding my social life.
+
+                DIY
+                I enjoy creating, fixing and mending. In the past I have created a woodstore, mended furniture and fixed various things around the house.
+            </p>
+        </div>
+    );
+}
